8e2251a475cfdd14a28e053626cb6be1953c4c79
Eddy-glitcher <luis-gonzales6@hotmail.com>
2023-10-21 11:21:41 -0500

    Feat: Added the user Route fields validator

---
+++ middlewares/field-validator.js
@@ -0,0 +1,24 @@
+// Con este middleware validamos que los campos requeridos que recibimos en la ruta POST /api/users, se reciban correctamente 
+
+const { response } = require('express');
+// Importamos el express validator
+const { validationResult }  = require('express-validator');
+
+
+const fieldValidator = (req , res= response , next)=>{ // si el middleware pasa ejecutamos el next
+    const errors = validationResult(req); // Crea un arreglo de errores capturados por el midleware
+    if ( !errors.isEmpty() ) {
+        return res.status(400).json({
+            ok: false,
+            errors : errors.mapped() // mapemaos los errores
+        })
+    }
+
+    // Llegados a este punto no hay errores.
+    next();
+
+}
+
+module.exports = {
+    fieldValidator
+}
