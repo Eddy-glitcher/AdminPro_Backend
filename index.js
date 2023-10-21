9541fc0ab7a03b7cbcadf0a379fb94e1c67b4809
Eddy-glitcher <luis-gonzales6@hotmail.com>
2023-10-21 11:25:04 -0500

    Refactor: Improve user routes

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 require('dotenv').config(); // leyemos variables de entorno y se establecen en las variables de entorno de node.js
 const cors = require('cors'); // Los coors son paquetes que permiten que mis endpoints acepten peticiones de cualquier dominio.
 
+// Rutas para los usuarios de la aplicacion. 
+const {userRouter} = require('./routes/users.routes'); 
+
 const { dbConnection } = require('./database/config'); // Importamos la configuración que me conecta node con mongo
 
 // Crear el Servidor de Express
@@ -9,19 +12,15 @@ const app = express();
 
 app.use(cors()); // Inicializamos los cors
 
+// Lectura y parseo del body. Recibimos los datos para crear el usuario
+app.use(express.json());
+
 // Base de datos
 dbConnection();
 
-// admin_mean_user
-// E06LZgApOujQJZAB
-
 // Rutas de la Aplicación
-app.get('/', (req,res)=>{
-    res.status(400).json({
-        ok : true,
-        msj : "Hola Mundo, Funciona!!"
-    });
-});
+// Las rutas las enlazamos con un midleware
+app.use('/api/users', userRouter );
 
 // Escuchamos peticiones al puerto especificado
 app.listen( process.env.PORT, ()=> {
